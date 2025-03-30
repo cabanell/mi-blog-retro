6c9dea9ab2ed97bf05c6a82a7185c4ff79b06af7
cabanell <rios86rogelio@icloud.com>
2025-03-30 14:47:51 -0600

    Actualizacion para utls

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,6 +1,3 @@
-from django.contrib import admin
-from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,5 +6,4 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
