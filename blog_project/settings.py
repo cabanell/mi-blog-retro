3ebf72d117843fd38541de98f51d7f97cfa3f18e
cabanell <rios86rogelio@icloud.com>
2025-03-30 14:31:44 -0600

    Actualizacion para media

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -85,5 +85,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
