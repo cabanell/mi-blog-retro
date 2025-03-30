51ff48c0cfc14cea876675a2d7ae4d76f874a01d
cabanell <rios86rogelio@icloud.com>
2025-03-30 14:53:26 -0600

    Corrijo error de importación y agrego media en producción

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -6,4 +8,5 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
+# Esto permite servir archivos media en producción también
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
