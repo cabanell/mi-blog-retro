c0cb43b13e26c18e6c52dfa7a7234d28d1243abe
cabanell <rios86rogelio@icloud.com>
2025-03-31 17:28:34 -0600

    Forzar serve para media en producción

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
 
-# Esta parte debe estar al final del archivo:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
