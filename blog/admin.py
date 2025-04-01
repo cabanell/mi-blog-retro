e78dad40751389d5a89faa10ef734ebc2a3499e0
cabanell <rios86rogelio@icloud.com>
2025-03-31 20:52:34 -0600

    se actualiza template sobre mi

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,12 @@ admin.site.register(Post)
 admin.site.register(Categoria)
 admin.site.register(Etiqueta)   
 admin.site.register(Comentario)
-admin.site.register(PerfilUsuario)  
+#admin.site.register(PerfilUsuario)  
 admin.site.register(ConfiguracionBlog)
 admin.site.register(ImagenGaleria)  
 admin.site.register(MensajeContacto)
 
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'nombre')
+    fields = ('usuario', 'nombre', 'biografia', 'foto')
